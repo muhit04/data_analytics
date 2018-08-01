82a1ec7ddfcec05a1df5d27f4a24bea8f0a3b3fc
Muhit Saleh Anik <muhit.anik@sydney.edu.au>
2018-08-01 12:52:18 +1000

    Pandas Load CSV-Header agnostic file into mysql

---
+++ pandas_csv_to_mysql.py
@@ -0,0 +1,23 @@
+## This script loads csv file in to db using pandas and sqlAlchemy
+## It is header agnostic and replaces the table if exists. 
+
+import pandas as pd
+from sqlalchemy import create_engine
+
+class CareerPath:
+    def __init__(self, path):
+        self.path = path
+        
+    def load_csv_file(self):
+        self.df = pd.read_csv(self.path)
+    
+    def load_val_into_db(self):
+        engine = create_engine("mysql://<username>:<password>@localhost/<dbname>")
+        con = engine.connect()
+        self.df.to_sql('<table_name>', con=con, if_exists='replace')
+    
+
+if __name__ == "__main__":
+    c = CareerPath("<csv_file_path>")
+    c.load_csv_file()
+    c.load_val_into_db()
